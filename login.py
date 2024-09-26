bf58f3b4caf3dbb0c5061f90d0ad0be214e6ff1a
Husaamuddeen <19rashidh@centralfoundationboys.co.uk>
2024-09-26 14:00:32 +0000

    code complete

--- login.py
+++ login.py
@@ -1,96 +1,70 @@
 import ast
 import random
-def userVerify():
-    verified = False
+
+
+def userVerify(user):
     users = open('users.txt','r')
     userID = input('please enter a 10 character ID: ')
     if len(userID) == 10:
-        print('a')
         for line in users:
             account = ast.literal_eval(line)
             if userID == account[0]:
-                print('verified')
-                verified = True
+                print('ID verified.')
                 user = account
                 users.close()
                 return user
     users.close()
-    return verified
-def PINVerify():
+    return user
+
+
+def PINVerify(user):
     PIN = int(user[1])
     PINtry = input('please enter the 4 digit PIN: ')
     if PINtry.isnumeric():
         if PIN == int(PINtry): 
             print('PIN accepted')
-            for i in range(3):
-                password = user[2]
-                index = random.randint(0,len(password)-1)
-                guess = input(f'enter the character in the position {} of your password')
-                if guess == password[index]:
-                    print('correct')
+            return 'success'
         else:
             print('incorrect PIN, access denied')
-            count = 3
-def login():
-    verified = False
-    count = 0
-    users = open('users.txt','r')
-    while verified != True and count < 3:
-        userID = input('please enter a 10 character ID: ')
-        if len(userID) == 10:
-            print('a')
-            for line in users:
-                account = ast.literal_eval(line)
-                if userID == account[0]:
-                    print('verified')
-                    verified = True
-                    user = account
-            print(verified)
-            if verified == False:
-                print('your ID does not exist.')
-                count = count + 1
-            else:
-                PIN = int(user[1])
-                PINtry = input('please enter the 4 digit PIN: ')
-                if PINtry.isnumeric():
-                    if PIN == int(PINtry): 
-                        print('PIN accepted')
-                        for i in range(3):
-                            password = user[2]
-                            index = random.randint(0,len(password)-1)
-                            guess = input(f'enter the character in the position {} of your password')
-                            if guess == password[index]:
-                                print('correct')
-                    else:
-                        print('incorrect PIN, access denied')
-                        count = 3
-                        
-        else:
-            print('your input was not 10 characters.')
-            count = count + 1
-    users.close()
+            return 'failed'
+        
 
-user = False
+def password(user):
+    count = 0
+    correct = 0
+    while count < 3:
+        password = user[2]
+        index = random.randint(0,len(password)-1)
+        guess = input(f'enter the character in the position {index+1} of your password: ')
+        if guess == password[index]:
+            correct = correct + 1
+        count = count + 1
+    if correct == 3:
+        return 'success'
+    else:
+        return 'failed'
+            
+
+user = 'unverified'
 count = 0
-while user == False and count < 3:
-    user = userVerify()
-    if user == False:
+while user == 'unverified' and count < 3:
+    user = userVerify(user)
+    if user == 'unverified':
         count = count + 1
     else:
-        PIN = int(user[1])
-        PINtry = input('please enter the 4 digit PIN: ')
-        if PINtry.isnumeric():
-            if PIN == int(PINtry): 
-                print('PIN accepted')
-                for i in range(3):
-                    password = user[2]
-                    index = random.randint(0,len(password)-1)
-                    guess = input(f'enter the character in the position {} of your password')
-                    if guess == password[index]:
-                        print('correct')
-            else:
-                print('incorrect PIN, access denied')
-                count = 3
-
-
-
+        PINResult = PINVerify(user)
+        if PINResult == 'failed':
+            count = 3
+        else:
+            count = 0
+            passwordCount = 0
+            while passwordCount < 3:
+                passwordCheck = password(user)
+                if passwordCheck == 'success':
+                    print('successfully logged in')
+                    passwordCount = 3
+                else:
+                    print('password failed, try again')
+                    passwordCount = passwordCount + 1
+if user == 'unverified':
+    print('unable to verify user ID, access denied')
