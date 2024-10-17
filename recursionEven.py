1f50e0c8e6d0ef56eb0175d7e8d9e7639da32e56
Husaamuddeen <19rashidh@centralfoundationboys.co.uk>
2024-10-17 13:25:05 +0000

    fibonacci

--- recursionEven.py
+++ recursionEven.py
@@ -12,4 +12,27 @@ def calcSum(n):
         sum = sum + i
         i = i + 1
     return sum
-print(calcSum(40))
+
+
+def fib(n):
+    if n <= 1:
+        return n
+    else:
+        return fib(n-1) + fib(n-2)
+
+def fibonacci2(n):
+    fibNumbers = [0,1]
+    for i in range(2,n+1):
+        fibNumbers.append(fibNumbers[i-1] + fibNumbers[i-2])
+    return fibNumbers[n]
+
+n = int(input('enter term: '))
+import time
+startTime = time.perf_counter()
+print(fib(n))
+endTime = time.perf_counter()
+print(endTime-startTime)
+startTime = time.perf_counter()
+print(fibonacci2(n))
+endTime = time.perf_counter()
+print(endTime-startTime)
