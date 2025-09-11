439de8e3b99e86b11bbcd83da14dc2347070d0e3
Husaamuddeen <19rashidh@centralfoundationboys.co.uk>
2025-09-11 12:00:21 +0000

    not

--- project/classes.py
+++ project/classes.py
@@ -11,3 +11,15 @@ class InputBox:
         if buttonrec.collidepoint((mouse_pos)):
             if pygame.mouse.get_pressed()[0]:
                 print('maths')
+
+#class to hold all the data about a triangle: sides, angles, area, perimeter
+class Triangle:
+    def __init__(self, _a, _b, _c, _A, _B, _C, _area, _perimeter):
+        self.a = _a
+        self.b = _b
+        self.c = _c
+        self.A = _A
+        self.B = _B
+        self.C = _C
+        self.area = _area
+        self.perimeter = _perimeter
--- project/pygametest.py
+++ project/pygametest.py
@@ -1,5 +1,31 @@
 import pygame
 from sys import exit
+import time
+
+class InputBox:
+    def __init__(self, inHeight, inWidth, inColour):
+        self.height = height
+        self.width = width
+        self.colour = colour
+        
+
+    def move(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if buttonrec.collidepoint((mouse_pos)):
+            if pygame.mouse.get_pressed()[0]:
+                print('maths')
+
+#class to hold all the data about a triangle: sides, angles, area, perimeter
+class Triangle:
+    def __init__(self, _a, _b, _c, _A, _B, _C, _area, _perimeter):
+        self.a = _a
+        self.b = _b
+        self.c = _c
+        self.A = _A
+        self.B = _B
+        self.C = _C
+        self.area = _area
+        self.perimeter = _perimeter
 
 #initialise pygame screen
 pygame.init()
@@ -9,24 +35,33 @@ clock = pygame.time.Clock()
 
 #set default font
 font = pygame.font.Font(None,50)
-a = int(input('enter a: '))
-b = int(input('enter b: '))
-c = int(input('enter c: '))
-if a >= b:
-    if a >= c:
-        longest = a
-    else:
-        longest = c
-else:
-    if b >= c:
+pygame.draw.line(screen,'blue',(800,0),(800,800))
+#pygame.draw.line(screen,'white',(0,400),(800,400))
+#pygame.draw.line(screen,'white',(400,0),(400,800))
+
+def constructTriangle():
+    a = 3
+    longest = a
+    workingTriangle = Triangle(a,None,None,None,None,None,None,None)
+    scale = 600/a
+    line_a = pygame.draw.line(screen, 'white', (50,550), (50+scale*a,550))
+    b = int(input(': '))
+    if b > longest:
+        line_a = 0
+        pygame.display.flip()
         longest = b
-        
+    
+
+#constructTriangle()
+
 
 button = pygame.Surface(((100,100)))
 button.fill('Blue')
 buttonrec = button.get_rect(center = (50,50))
 print(buttonrec)
 #main animation loop
+count = 0
+def mainMenu
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -34,8 +69,12 @@ while True:
             exit()
 
     #update surfaces
-    screen.blit(button,buttonrec)
-
+    count += 1
+    if count < 100:
+        screen.blit(button,buttonrec)
+    else:
+        screen.fill('black')
+        print('gone')
     #check for mouse collisions and mouse presses
     mouse_pos = pygame.mouse.get_pos()
     if buttonrec.collidepoint((mouse_pos)):
