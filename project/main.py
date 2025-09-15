c3bf1800c4a732dcf3f17611120afd2ac5bb04cb
Husaamuddeen <19rashidh@centralfoundationboys.co.uk>
2025-09-15 10:06:34 +0000

    images

--- project/main.py
+++ project/main.py
@@ -61,11 +61,16 @@ class Button:
         self.surface.fill(self.colour)
         self.rect = self.surface.get_rect(center = self.centre)
 
+class Image:
+    def __init__(self, _path, _centre):
+        self.centre = _centre
+        self.surface = pygame.image.load(_path)
+        self.rect = self.surface.get_rect(center = self.centre)
+
 #main animation loop
 def mainMenu():
     screen.fill('black')
-
-    button1 = Button(100,300,'blue',(400,200))
+    buttons = [Image('project\\images\options.png',(500,400)),Image('project\\images\modules.png',(600,400)),Image('project\\images\saves.png',(700,400))]
 
     while True:
         for event in pygame.event.get():
@@ -74,13 +79,54 @@ def mainMenu():
                 exit()
 
         #update surfaces
-        screen.blit(button1.surface,button1.rect)
+        screen.blit(buttons[0].surface, buttons[0].rect)
 
         #check for mouse collisions and mouse presses
         mouse_pos = pygame.mouse.get_pos()
-        if button1.rect.collidepoint((mouse_pos)):
-            if pygame.mouse.get_pressed()[0]:
-                print('maths')
+        for i in range(len(buttons)):
+            screen.blit(buttons[i].surface, buttons[i].rect)
+            if buttons[i].rect.collidepoint((mouse_pos)):
+                if pygame.mouse.get_pressed()[0]:
+                    match i:
+                        case 0:
+                            print('options')
+                        case 1:
+                            modules()
+                        case 2:
+                            print('settings')
+
+        pygame.display.update()
+        clock.tick(60)
+
+def modules():
+    screen.fill('black')
+    buttons = [Button(100,250,'yellow',(300,200)),Button(100,250,'yellow',(600,200)),Button(100,250,'yellow',(900,200)),
+               Button(100,250,'yellow',(300,400)),Button(100,250,'yellow',(600,400)),Button(100,250,'yellow',(900,400)),
+               Button(100,250,'yellow',(300,600)),Button(100,250,'yellow',(600,600)),Button(100,250,'yellow',(900,600)),
+               Image('project\\images\\back.png',(1000,700))]
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        #update surfaces
+        screen.blit(buttons[0].surface, buttons[0].rect)
+
+        #check for mouse collisions and mouse presses
+        mouse_pos = pygame.mouse.get_pos()
+        for i in range(len(buttons)):
+            screen.blit(buttons[i].surface, buttons[i].rect)
+            if buttons[i].rect.collidepoint((mouse_pos)):
+                if pygame.mouse.get_pressed()[0]:
+                    match i:
+                        case 0:
+                            print('options')
+                        case 1:
+                            print('modules')
+                        case 9:
+                            mainMenu():
 
         pygame.display.update()
         clock.tick(60)
